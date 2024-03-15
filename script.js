00cd24bb34675a9842b19cba1cc1e5ba25f0aecf
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2024-03-15 19:19:33 +0100

    Fixed win counter functionality

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function getComputerChoice() {
 
     let randomnr = Math.random();
 
-    if (randomnr < (1/3)) {
+    if (randomnr < (1 / 3)) {
         return rock;
-    } else if (randomnr <= (2/3)){
+    } else if (randomnr <= (2 / 3)) {
         return scissors;
-    } else if (randomnr > (2/3)){
+    } else if (randomnr > (2 / 3)) {
         return paper;
     }
 }
@@ -18,31 +18,48 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
         return ("It's a tie! The Computer chose: " + computerSelection);
-    } else if (playerSelection.toLowerCase() == "rock"  && computerSelection.toLowerCase() == "scissors") {
-        return("You won! The Computer chose: " + computerSelection);
-    } else if (playerSelection.toLowerCase() == "rock"  && computerSelection.toLowerCase() == "paper") {
-        return("You lost! The Computer chose: " + computerSelection);
-    } else if (playerSelection.toLowerCase() == "paper"  && computerSelection.toLowerCase() == "scissors") {
-        return("You lost! The Computer chose: " + computerSelection);
-    } else if (playerSelection.toLowerCase() == "paper"  && computerSelection.toLowerCase() == "rock") {
-        return("You Won! The Computer chose: " + computerSelection);
-    } else if (playerSelection.toLowerCase() == "scissors"  && computerSelection.toLowerCase() == "rock") {
-        return("You lost! The Computer chose: " + computerSelection);
-    } else if (playerSelection.toLowerCase() == "scissors"  && computerSelection.toLowerCase() == "paper") {
-        return("You Won! The Computer chose: " + computerSelection);
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors") {
+        return ("You won! The Computer chose: " + computerSelection);
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper") {
+        return ("You lost! The Computer chose: " + computerSelection);
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors") {
+        return ("You lost! The Computer chose: " + computerSelection);
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock") {
+        return ("You Won! The Computer chose: " + computerSelection);
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock") {
+        return ("You lost! The Computer chose: " + computerSelection);
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper") {
+        return ("You Won! The Computer chose: " + computerSelection);
     }
 }
 
 
 
 function playGame(userinput, computerinput) {
+
+    let wins = 0;
+
     for (let index = 0; index < 5; index++) {
+        
         userinput = prompt("What is you choice?");
         playerselect = userinput.toString();
         computerinput = getComputerChoice();
+        let result = playRound(playerselect, computerinput);
 
-        console.log(playRound(playerselect, computerinput));
+        console.log(result);
+
+        if (result === "You won! The Computer chose: " + computerinput) {
+            wins++;
+        }
+    }
+
+    if (wins >= 3){
+        console.log("You are the winner! You won " + wins + " times.")
+    } else if (wins == 1) {
+        console.log("Sorry, you lost! You only won " + wins + " time.")
+    } else {
+        console.log("Sorry, you lost! You only won " + wins + " times.")
     }
 }
 
-// playGame();
+playGame();
